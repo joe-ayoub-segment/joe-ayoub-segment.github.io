215f02a8b7c64a4cf4968c3f36316de46d815e3a
joe-ayoub-segment <45374896+joe-ayoub-segment@users.noreply.github.com>
2022-09-01 15:24:48 +0200

    Create arduino.cc

---
+++ arduino.cc
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html>
+<head>
+  <title>Arduno test page</title>
+
+<script>
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://evs.aayinltcs.arduino.cc/5yy37B6M2UBwaTzKNi37Ls/jw69SnrNvkn85VzQoL6KM7.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="x4EubRJSN0dxw7rUcitr4F6eSxFnUEmS";analytics._cdn = "https://evs.aayinltcs.arduino.cc";analytics.SNIPPET_VERSION="4.15.3";
+  analytics.load("x4EubRJSN0dxw7rUcitr4F6eSxFnUEmS");
+  analytics.page();
+  }}();
+</script>
+
+</head>
+
+<body>
+    <p>Empty page to test sending GA conversion events</p>
+</body>
+
+</html>
